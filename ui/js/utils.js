f88c264d4a81434ecc3dac5591f10cc0d2a09412
Panu Ranta <panu.ranta@iki.fi>
2016-02-11 19:03:00 +0200

    Improved visibility of long route names in vehicle icon text.

--- ui/js/utils.js
+++ ui/js/utils.js
@@ -47,7 +47,7 @@ function Utils() {
     this.setDomTooltipPosition = function (tooltipElement, rect) {
         var leftOffset = {true: 0,
             false: tooltipElement.offsetWidth}[rect.left < (window.innerWidth / 2)];
-        tooltipElement.style.left = (rect.left - leftOffset) + 'px';
+        tooltipElement.style.left = Math.max(0, rect.left - leftOffset) + 'px';
         var topOffset = {true: -rect.height,
             false: tooltipElement.offsetHeight}[rect.top < (window.innerHeight / 2)];
         tooltipElement.style.top = (rect.top - topOffset) + 'px';
