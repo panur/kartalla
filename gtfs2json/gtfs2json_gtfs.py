f598e02f614ae2ad35ec2f36bbf443cf7d3a571d
Panu Ranta <panu.ranta@iki.fi>
2016-06-23 18:39:21 +0300

    Updated GTFS agency_ids based on Liikennevirasto's new (2016-06-19) data.

--- gtfs2json/gtfs2json_gtfs.py
+++ gtfs2json/gtfs2json_gtfs.py
@@ -70,8 +70,8 @@ def _parse_stops(stops_txt):
 def _parse_routes(routes_txt):
     routes = {}  # by route_id
     # 109: https://github.com/HSLdevcom/kalkati2gtfs/commit/d4758fb74d7455ddbf4032175ef8ff51c587ec7f
-    route_types = {'0': 'tram', '1': 'metro', '3': 'bus', '4': 'ferry', '109': 'train',
-                   '2': 'train', '704': 'bus', '1104': 'airplane'}
+    route_types = {'0': 'tram', '1': 'metro', '3': 'bus', '4': 'ferry', '106': 'train',
+                   '109': 'train', '2': 'train', '704': 'bus', '1104': 'airplane'}
 
     with open(routes_txt, 'r') as input_file:
         csv_reader = csv.DictReader(input_file)
